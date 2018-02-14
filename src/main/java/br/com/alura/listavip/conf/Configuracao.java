2b8e0914613440f716333149237fe43173a4860e
Jon Ribeiro <jonathas02@gmail.com>
2018-02-15 00:43:07 +0100

    Isolated the code to run the system on a Main class

--- src/main/java/br/com/alura/listavip/conf/Configuracao.java
+++ src/main/java/br/com/alura/listavip/conf/Configuracao.java
@@ -2,29 +2,22 @@ package br.com.alura.listavip.conf;
 
 import javax.sql.DataSource;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
-@Controller
+@Component
 public class Configuracao {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Configuracao.class, args);
-	}
 
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-		dataSource.setUrl("root");
-		dataSource.setPassword("");
+		dataSource.setUsername("root");
+        dataSource.setPassword("");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 
 		return dataSource;
 	}
-
+	
 }

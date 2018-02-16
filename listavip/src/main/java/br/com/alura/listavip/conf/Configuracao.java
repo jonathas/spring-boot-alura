fbebc728a96baa633de78187fec889be9286c273
Jon Ribeiro <jonathas02@gmail.com>
2018-02-16 06:41:13 +0100

    Added an application.properties file with server port and db configuration

--- listavip/src/main/java/br/com/alura/listavip/conf/Configuracao.java
+++ listavip/src/main/java/br/com/alura/listavip/conf/Configuracao.java
@@ -1,23 +1,8 @@
 package br.com.alura.listavip.conf;
 
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Configuracao {
-
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/listavip");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-
-		return dataSource;
-	}
 	
 }

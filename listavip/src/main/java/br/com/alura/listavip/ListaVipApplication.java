fa180236b700aa0b509511f87a497128c1d8f8d8
Jon Ribeiro <jonathas02@gmail.com>
2018-02-16 07:10:08 +0100

    Removed unecessary annotation

--- listavip/src/main/java/br/com/alura/listavip/ListaVipApplication.java
+++ listavip/src/main/java/br/com/alura/listavip/ListaVipApplication.java
@@ -2,10 +2,8 @@ package br.com.alura.listavip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 public class ListaVipApplication {
 
 	public static void main(String[] args) {
